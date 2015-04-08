7081128d121c057cae6ad5a300bd2758b8ef4a2d
David Luke Gordon <dlgordon@calpoly.edu>
2015-04-08 14:48:55 -0700

    Refactored SQLiteJDBC.java

--- implementation/src/SQLiteJDBC.java
+++ implementation/src/SQLiteJDBC.java
@@ -1,23 +1,26 @@
 import java.sql.*;
 
 public class SQLiteJDBC {
+   private static final String db_name = "test.db"
+   private static final String create_students_table = 
+    "CREATE TABLE students (username TEXT PRIMARY KEY, first TEXT NOT NULL, last TEXT NOT NULL);";
+
    public static void main(String args[]) {
       Connection c = null;
       try {
          Class.forName("org.sqlite.JDBC");
-         c = DriverManager.getConnection("jdbc:sqlite:test.db");
-         init_tables(c);
+         c = DriverManager.getConnection("jdbc:sqlite:" + db_name);
+         create_tables(c);
       } catch ( Exception e ) {
          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-         System.exit(0);
+         System.exit(1);
       }
       System.out.println("Opened database successfully");
    } 
-   public static void init_tables(Connection c) throws Exception { 
+   
+   public static void create_tables(Connection c) throws Exception { 
       Statement s = c.createStatement();
-      String query =
-       "CREATE TABLE students (username TEXT PRIMARY KEY, first TEXT NOT NULL, last TEXT NOT NULL);";
-      s.executeUpdate(query);
+      s.executeUpdate(create_students_table);
       s.close();
    }
 }
