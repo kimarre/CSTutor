b9e5a5e9d5300b4483bda3ff91ff956b01f44d91
svurens <svurens@gmail.com>
2015-04-13 15:02:07 -0700

    Chat GUI

---
+++ implementation/Chat/ChatDriver.java
@@ -0,0 +1,23 @@
+package Chat;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+
+public class ChatDriver extends JFrame{
+	//public static 
+	public static ChatDriver thisOne;
+	public static void main(String[] args) {
+		thisOne = new ChatDriver();
+	}
+	public ChatDriver() {
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setSize(5, 100);
+		this.setLayout(new BorderLayout());
+		this.add(new ChatBar(), BorderLayout.SOUTH);
+		this.add(new JLabel("Hello World"), BorderLayout.CENTER);
+		//this.add(box);
+		this.setSize(500, 300);
+		//this.pack();
+	}
+}
