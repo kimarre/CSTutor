d23fbf9dc5df9fc7eb8fd0e0120776b7a1e8d90d
Kim Arre <kimarrexo@gmail.com>
2015-04-13 16:31:28 -0700

    Fixed Simon's packages.

--- implementation/src/java/CSTutor/View/Manager/SectionStudentPicker.java
+++ implementation/src/java/CSTutor/View/Manager/SectionStudentPicker.java
@@ -1,4 +1,4 @@
-package Manager;
+package CSTutor.View.Manager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- implementation/src/java/CSTutor/View/Manager/StudentPicker.java
+++ implementation/src/java/CSTutor/View/Manager/StudentPicker.java
@@ -1,4 +1,4 @@
-package Manager;
+package CSTutor.View.Manager;
 
 import java.awt.*;
 import java.awt.event.*;
