b3422a808fc94bbd330b51264f3bfb7a94fbf840
Stephen Daily <dddstephen@gmail.com>
2015-04-20 21:55:56 -0700

    Made more changes to chat files.

--- implementation/src/java/CSTutor/Model/Chat/StudentText.java
+++ implementation/src/java/CSTutor/Model/Chat/StudentText.java
@@ -1,5 +1,7 @@
 package CSTutor.Model.Chat;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
 /**
  * studentText is a class that will be used by the Console class. It's an object
  * that encompasses a text string, and some student data to output information to
@@ -15,20 +17,27 @@ public class StudentText {
     //The student's screan name.
     int studentID;
     //The time at which the input was given
-    String time;
+    Date time;
     
     
     /**
      * Constructor
      */
-    public StudentText() {
-    	
+    public StudentText(String text) {
+    	this.text = text;
+    	this.studentID = "123456";
+    	this.time = new Date();
+    }
+    
+    private string formatTime() {
+    	SimpleDateFormat ft = new SimpleDateFormat ("HH.mm.ss a");
+    	return ft.format(time);
     }
     
     /**
      * Converts a regular string into student text. 
      */
-    public StudentText strToStudentText(String s) {
-    	return null;
+    public string strToStudentText(String s) {
+    	return (formatTime() + studentID + text);
     }
 }
