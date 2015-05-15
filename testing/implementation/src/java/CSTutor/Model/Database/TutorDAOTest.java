d73fd09130c5dbc48fc64c475c5fea774666dc39
David Luke Gordon <dlgordon@calpoly.edu>
2015-05-14 22:01:30 -0700

    Added test class for TutorDAO

---
+++ testing/implementation/src/java/CSTutor/Model/Database/TutorDAOTest.java
@@ -0,0 +1,24 @@
+package CSTutor.Model.Database;
+
+import org.junit.*;
+
+public class TutorDAOTest {
+   public TutorDAOTest() {
+   }
+
+   @BeforeClass
+   public static void setUpClass() {
+   }
+    
+   @AfterClass
+   public static void tearDownClass() {
+   }
+    
+   @Before
+   public void setUp() {
+   }
+    
+   @After
+   public void tearDown() {
+   }
+}
