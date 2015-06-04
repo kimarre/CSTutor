dace462bd7239557cbc82d02ab0d3429f56d0c5c
Erica Solum <erica.solum@yahoo.com>
2015-06-04 14:14:47 -0700

    Adding QuizData class

---
+++ implementation/src/java/CSTutor/Model/Progress/QuizData.java
@@ -0,0 +1,29 @@
+package CSTutor.Model.Progress;
+
+public class QuizData
+{
+    private String name;
+    private int numQuestions;
+    private int quizID;
+    
+    public QuizData(String quizName, int qID, int numQ)
+    {
+        this.name = quizName;
+        numQuestions = numQ;
+        quizID = qID;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public int getNumQuestions()
+    {
+        return numQuestions;
+    }
+    
+    
+    
+
+}
