a224f896256b70e2a0de7fcddb3b2eea9ad35bf7
Stephen Daily <ssdaily@calpoly.edu>
2015-06-11 21:53:25 -0700

    Added tests for chat.

---
+++ testing/implementation/src/java/CSTutor/Model/Chat/Client/ChatClientTest.java
@@ -0,0 +1,29 @@
+package CSTutor.Model.Chat.Client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChatClientTest {
+
+	@Test
+	public void testChatClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecieveMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBroadcastMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ testing/implementation/src/java/CSTutor/Model/Chat/Server/ChatServerTest.java
@@ -0,0 +1,33 @@
+package CSTutor.Model.Chat.Server;
+
+import static org.junit.Assert.*;
+
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+import CSTutor.Model.Chat.Server.ChatServer;
+
+public class ChatServerTest {
+	
+	ChatServer cs;
+	@Test
+	public void testChatServer() {
+		cs = new ChatServer();
+		assertTrue("ChatServer contrsuctor works: ", cs != null);
+	}
+
+	@Test
+	public void testBroadcastMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterChatClient() {
+		ObjectOutputStream os = new ObjectOutputStream()
+		cs.registerChatClient(os);
+		assertTrue("Register chat client works: ", cs.chatClients.contains(os));
+		cs.chatClients.remove(os);
+	}
+
+}
