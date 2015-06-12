fc4cfaaa81fdc49de8c7ecc803dce0d78c59bb98
fedorareis <fedorareis@gmail.com>
2015-06-11 17:46:05 -0700

    Updated Overview and User views

--- implementation/src/java/CSTutor/View/Overview.java
+++ implementation/src/java/CSTutor/View/Overview.java
@@ -19,28 +19,43 @@ import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Overview extends JFrame implements Observer {
+public class Overview extends JFrame {
     private JMenuBar jMenuBar;
     private JMenu jMenuHome;
     private JMenu jMenuTutorials;
     private JMenu jMenuQuizzes;
     private JMenu jMenuProgress;
     private JMenu jMenuLogin;
+    private JMenu jMenuProfile;
     private JMenuItem myTuts;
     private JMenuItem createTuts;
     private JMenuItem myQuiz;
     private JMenuItem createQuiz;
+    private JMenuItem profile;
+    private JMenuItem logout;
+    private JMenuItem admin;
     private Login login;
     private JPanel mainPanel;
     private JPanel mainTop;
     private JPanel insideTop;
     private ChatBar chatBar;
+    private JLabel userInfo;
     public User user;
     private static int INSTRUCTOR_ACCESS = 2;
-    
+    private static int STUDENT_ACCESS = 1;
+
+    /**
+     * The programs main class.
+     *
+     * @param args Any arguments passed in at runtime.
+     */
     public static void main(String[] args) {
         new Overview();
     }
+
+    /**
+     * The constructor for the Overview class.
+     */
     public Overview() {
         /*
     	try
@@ -80,19 +95,11 @@ public class Overview extends JFrame implements Observer {
         add(mainPanel);
         pack();
         setVisible(true);
-
-        System.out.println("Componenets");
-        for(Component comp : mainTop.getComponents())
-        {
-            System.out.println(comp.toString());
-        }
     }
 
-    private void addObservables()
-    {
-        //login.addObserver(this);
-    }
-   
+    /**
+     * initializes all of the components for the Overview frame.
+     */
     private void init()
     {
         jMenuBar = new JMenuBar();
@@ -104,7 +111,12 @@ public class Overview extends JFrame implements Observer {
         myTuts = new JMenuItem();
         createTuts = new JMenuItem();
         myQuiz = new JMenuItem();
+        profile = new JMenuItem();
+        logout = new JMenuItem();
+        admin = new JMenuItem();
         createQuiz = new JMenuItem();
+        userInfo = new JLabel();
+
 
         // Home
         jMenuHome.setText("Home");
@@ -153,35 +165,73 @@ public class Overview extends JFrame implements Observer {
                 LoginAction(evt);
             }
         });
+        jMenuBar.add(new JSeparator(SwingConstants.VERTICAL));
+        userInfo.setText("     You are currently a Guest.     ");
+        jMenuBar.add(userInfo);
+        jMenuBar.add(new JSeparator(SwingConstants.VERTICAL));
         jMenuBar.add(jMenuLogin);
         
         setJMenuBar(jMenuBar);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     }
-    
+
+    /**
+     * Launches the login dialog.
+     *
+     * @param evt A mouse click.
+     */
     private void LoginAction(java.awt.event.MouseEvent evt) {                             
         login = new Login(this);
         login.setVisible(true);
     }
 
-    private void LogoutAction(java.awt.event.MouseEvent evt) {
+    /**
+     * Launches the profile dialog.
+     *
+     * @param evt A mouse click.
+     */
+    private void ProfileAction(java.awt.event.ActionEvent evt) {
+        (new Profile(user)).setVisible(true);
+    }
+
+    /**
+     * Launches the Admin Panel dialog.
+     *
+     * @param evt A mouse click.
+     */
+    private void AdminAction(java.awt.event.ActionEvent evt) {
+        (new AdminPanel()).setVisible(true);
+    }
+
+    /**
+     * Changes the Manager state back to the guest state
+     *
+     * @param evt A mouse click.
+     */
+    private void LogoutAction(java.awt.event.ActionEvent evt) {
         user = null;
-        jMenuLogin.setText("Login");
-        jMenuLogin.removeMouseListener(jMenuLogin.getMouseListeners()[2]);
-        jMenuLogin.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                LoginAction(evt);
-            }
-        });
+        userInfo.setText("     You are currently a Guest.     ");
+        jMenuBar.remove(jMenuProfile);
+        jMenuBar.add(jMenuLogin);
         jMenuProgress.removeMouseListener(jMenuProgress.getMouseListeners()[2]);
         jMenuProgress.setEnabled(false);
         jMenuTutorials.remove(createTuts);
         jMenuQuizzes.remove(createQuiz);
+        jMenuLogin.remove(logout);
+        jMenuLogin.remove(profile);
+        jMenuLogin.remove(admin);
         chatBar.disableButtons();
+        jMenuBar.revalidate();
+        jMenuBar.repaint();
         revalidate();
         repaint();
     }
-    
+
+    /**
+     * Creates a new instance of the Manager screen and displays it when Home is selected from the menu.
+     *
+     * @param evt A mouse click.
+     */
     private void HomeAction(java.awt.event.MouseEvent evt) {
         mainTop.remove(0);
         mainTop.add(new ManagerGUI(), "Manager", 0);
@@ -190,7 +240,12 @@ public class Overview extends JFrame implements Observer {
         repaint();
         ((CardLayout)(mainTop.getLayout())).show(mainTop, "Manager");
     }
-    
+
+    /**
+     * Creates a new instance of the Tutorial screen and displays it when My Tutorials is selected from the menu.
+     *
+     * @param evt A mouse click.
+     */
     private void MyTutorialAction(java.awt.event.ActionEvent evt) {
         mainTop.remove(3);
         mainTop.add(new Tutorial(), "Student Tutorial", 3);
@@ -200,7 +255,12 @@ public class Overview extends JFrame implements Observer {
        // roadmapList.setSelectedIndex(1);
         ((CardLayout)(mainTop.getLayout())).show(mainTop, "Student Tutorial");
     }
-    
+
+    /**
+     * Creates a new instance of the Tutorial Creator screen and displays it when Create Tutorial is selected from the menu.
+     *
+     * @param evt A mouse click.
+     */
     private void CreateTutorialAction(java.awt.event.ActionEvent evt) {
         mainTop.remove(2);
         mainTop.add(new CSTutor.View.Tutorial.EditTutorial(), "Tutorial", 2);
@@ -209,7 +269,12 @@ public class Overview extends JFrame implements Observer {
         repaint();
         ((CardLayout)(mainTop.getLayout())).show(mainTop, "Tutorial");
     }
-    
+
+    /**
+     * Creates a new instance of the Quiz screen and displays it when Quiz is selected from the menu.
+     *
+     * @param evt A mouse click.
+     */
     private void QuizAction(java.awt.event.ActionEvent evt) {
         mainTop.remove(4);
         mainTop.add(new QuizBuildGUI(), "Quiz", 4);
@@ -218,25 +283,53 @@ public class Overview extends JFrame implements Observer {
         repaint();
         ((CardLayout)(mainTop.getLayout())).show(mainTop, "Quiz");
     }
-    
+
+    /**
+     * Creates a new instance of the Progress screen and displays it when Progress is selected from the menu.
+     *
+     * @param evt A mouse click.
+     */
     private void ProgressAction(java.awt.event.MouseEvent evt) {
         mainTop.remove(1);
-        mainTop.add(new Progress(INSTRUCTOR_ACCESS).getView(), "Progress", 1);
+        if(user.isInstructor()) {
+            mainTop.add(new Progress(INSTRUCTOR_ACCESS).getView(), "Progress", 1);
+        } else {
+            mainTop.add(new Progress(STUDENT_ACCESS).getView(), "Progress", 1);
+        }
         pack();
         revalidate();
         repaint();
         ((CardLayout)(mainTop.getLayout())).show(mainTop, "Progress");
     }
 
+    /**
+     * What to do when a user is logged in. Changes the state of the overview frame to reflect the change in login state.
+     */
     public void LoggedIn()
     {
-        jMenuLogin.setText("Logout");
-        jMenuLogin.removeMouseListener(jMenuLogin.getMouseListeners()[2]);
-        jMenuLogin.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
+
+        jMenuProfile = new JMenu();
+        userInfo.setText("     You are currently logged in as " + user.getName() + ".     ");
+        jMenuBar.remove(jMenuLogin);
+
+        jMenuProfile.setText("Profile");
+        profile.setText("My Profile");
+        profile.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ProfileAction(evt);
+            }
+        });
+        jMenuProfile.add(profile);
+
+        logout.setText("Logout");
+        logout.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 LogoutAction(evt);
             }
         });
+
         jMenuProgress.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 ProgressAction(evt);
@@ -258,11 +351,20 @@ public class Overview extends JFrame implements Observer {
             createQuiz.setText("Create Quiz");
             jMenuQuizzes.add(createQuiz);
 
-            revalidate();
-            repaint();
+            admin.setText("Admin Panel");
+            admin.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    AdminAction(evt);
+                }
+            });
+            jMenuProfile.add(admin);
         }
-    }
 
-    public void update(Observable o, Object arg) {
+        jMenuProfile.add(logout);
+        jMenuBar.add(jMenuProfile);
+        jMenuBar.revalidate();
+        jMenuBar.repaint();
+        revalidate();
+        repaint();
     }
 }
