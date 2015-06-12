b38c68366f46e71ab4cf381c619851eb114d8cfa
Kim Arre <kimarrexo@gmail.com>
2015-06-11 20:12:29 -0700

    Added CodeExample Test

---
+++ testing/implementation/src/java/CSTutor/Model/Tutorial/CodeExampleTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CSTutor.Model.Tutorial;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kimrawr
+ */
+public class CodeExampleTest {
+    
+    public CodeExampleTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        CodeExample test = new CodeExample();
+        
+    }
+    
+}
